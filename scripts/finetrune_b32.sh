4f050c2bbd6bb84e18bc423c8b0e6f4d53088de4
jiaaozhe <jiaaozhe1999@outlook.com>
2024-10-29 09:47:44 +0800

    add finetuning script

---
+++ scripts/finetrune_b32.sh
@@ -0,0 +1,25 @@
+# specify which GPUs you want to use.
+export CUDA_VISIBLE_DEVICES=0,1
+cd /home/yajun/projects/jiaaozhe/open_clip/src
+# set the training args
+
+torchrun --nproc_per_node 2 -m open_clip_train.main \
+    --save-most-recent \
+    --seed 1234 \
+    --batch-size 200 \
+    --precision amp \
+    --workers 4 \
+    --report-to tensorboard \
+    --save-frequency 1 \
+    --dataset-type jsonl \
+    --csv-separator="," \
+    --train-data "./dataset/train.jsonl"\
+    --val-data "./dataset/test.jsonl" \
+    --csv-img-key image_path \
+    --csv-caption-key caption \
+    --warmup 1000 \
+    --lr=5e-6 \
+    --wd=0.1 \
+    --epochs=200 \
+    --model xlm-roberta-large-ViT-H-14 \
+    --pretrained "../pretrained_models/openclip-xlm-vit-b32.bin"
